ed6764129183bdf95cb3255e843308472d2fb412
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 10:01:02 -0700

    Q 04 ts/js of 45 assignment
    Q 04 ts/js of 45 assignment

---
+++ Q4.ts
@@ -0,0 +1,6 @@
+//Q4.Famous Quote:Find a quote from a famous person you admire.
+//Print the quote and the name of its author.Your output should look something like the following, 
+//including the quotation marks:
+let authorname:string =("The wizard of oz,1939");
+console.log(`${authorname},"there is no place like home" `)
+
